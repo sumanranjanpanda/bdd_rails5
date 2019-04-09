673351fc89e4de5d7b4bca1963de99b0b441a55a
Suman Ranjan Panda <sumanranjanpanda@gmail.com>
2019-04-09 10:37:08 +0530

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(email: 'suman.panda@mindfiresolutions.com', password: 'password')
+user.articles.create!(title: "RSpec", body: "RSpec is a unit test framework for the Ruby programming language. RSpec is different than traditional xUnit frameworks like JUnit because RSpec is a Behavior driven development tool.")
+user.articles.create!(title: "Capybara", body: "Capybara is a web-based test automation software that simulates scenarios for user stories and automates web application testing for behavior-driven software development. It is written in the Ruby programming language. Capybara can mimic actions of real users interacting with web-based applications.")
